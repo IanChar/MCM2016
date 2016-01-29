25b7cc0f026a2aa7122cd8ed5ba2e7933ab65ba3
Ji Hoon Kim <jkim404330@gmail.com>
2016-01-29 16:16:49 -0700

    Improved parsing on Lumina

--- gates.py
+++ gates.py
@@ -1,10 +1,22 @@
-from lxml import html
-import requests
+from requests import Session
 
-page = requests.get('http://www.gatesfoundation.org/How-We-Work/Quick-Links/Grants-Database#q/k=university&program=US%20Program')
-tree = html.fromstring(page.content)
+session = Session()
 
-#This will create a list of buyers:
-buyers = tree.xpath('//a')
+# HEAD requests ask for *just* the headers, which is all you need to grab the
+# session cookie
+session.head('http://www.gatesfoundation.org/How-We-Work/Quick-Links/Grants-Database')
 
-print 'Buyers: ', buyers
+response = session.post(
+    url='http://www.gatesfoundation.org/services/gfo/search.ashx',
+    data={
+        'N': '4294966750',
+        'form-trigger': 'moreId',
+        'moreId': '156#327',
+        'pageType': 'EventClass'
+    },
+    headers={
+        'Referer': 'http://www.gatesfoundation.org/How-We-Work/Quick-Links/Grants-Database'
+    }
+)
+
+print response.text
--- lumina.py
+++ lumina.py
@@ -1,10 +1,36 @@
 from lxml import html
 import requests
+from itertools import chain, izip
 
-page = requests.get('https://www.luminafoundation.org/grants-database/search/?q=university')
-tree = html.fromstring(page.content)
-print tree.text_content()
-#This will create a list of buyers:
-buyers = tree.xpath('//div[@class="grant-detail"]/strong/text()')
 
-print 'Buyers: ', buyers
+class Lumina:
+
+    def __init__(self):
+        self.acc = []
+        self.acc2 = []
+
+    def findGrants(self):
+        for i in range(1,29):
+            page = requests.get('https://www.luminafoundation.org/grants-database/search/?q=university&pg=' + str(i))
+            tree = html.fromstring(page.content)
+            #This will create a list of buyers:
+            grantInfo = tree.xpath('//div[@class="grant-detail"]/strong/text()')
+            grantName = tree.xpath('//div[@class="grant-item clearfix"]/h1/a/text()')
+
+            for i, x in enumerate(range(0, len(grantInfo), 4)):
+                self.acc = self.acc + [[str(grantName[i])] + grantInfo[x:x+4]]
+
+            for item in self.acc:
+                item = item[0:4] + self.findYears(item[4])
+                self.acc2.append(item)
+
+        return self.acc2
+
+    def findYears(self, item):
+        item = item.replace("/", " ")
+        item = item.split()
+        return [item[2], item[6]]
+
+L = Lumina()
+
+print L.findGrants()
