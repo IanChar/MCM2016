f16852f1b52b0cc6129b2c312381ca4b795fca16
IanChar <ianchar4@gmail.com>
2016-01-30 19:33:56 -0700

    Made Summary func

--- QueryDB.py
+++ QueryDB.py
@@ -47,8 +47,74 @@ def getAllRecent():
     cnx.close()
     return res
 
+def getSummarySheet(data, extensive = False, year=2013):
+    cnx = mysql.connector.connect(user=USERNAME, password=PASSWORD,
+            host="127.0.0.1", database="MCM")
+    cursor = cnx.cursor()
+    if extensive:
+        query = ("SELECT * FROM"
+                " (SELECT * FROM School NATURAL JOIN Location) AS A"
+                " NATURAL JOIN (SELECT * FROM TimeSlice WHERE year=%s) AS B")
+        cursor.execute(query, tuple([year]))
+    else:
+        query = ("SELECT * FROM School NATURAL JOIN Location")
+        cursor.execute(query)
+    res = []
+    for c in cursor:
+        res.append(c)
+    rankCount = 1
+    for d in data:
+        dbInfo = 0
+        for r in res:
+            if r[2] == d[0]:
+                dbInfo = r
+                break
+        print "------------------------------------------------"
+        print "Name:", str(dbInfo[2])
+        print "Rank:", rankCount
+        rankCount += 1
+        print "Score:", d[1]
+        print "Location:", ", ".join(map(str, dbInfo[3:5]))
+        if extensive:
+            print "Year:", year
+            printCounter = 6
+            for msg in MESSAGES:
+                print msg, dbInfo[printCounter]
+                printCounter += 1
+    print "------------------------------------------------"
+
+MESSAGES = ["Undergraduates:", "Undergraduate (non-resident aliens:",
+         "Undergraduates (unknown race):", "White Undergraduates:",
+        "Black Undergraduates:", "Asian Undergraduates:",
+        "Native American Undergraduates:",
+        "Hispanic Undergraduates:", "In-state Tuition:",
+        "Out-of-state Tuition:", 
+        "Net Tuition Revenue (per student):",
+        "Instructional Expenditures (per student):",
+        "Adjusted Cohort Count Completion Rate:",
+        "Percent Completed in 2 Years:",
+        "Percent Withdrawn after 2 Years:",
+        "Percent Completed in 2 Years (Female):",
+        "Percent Withdrawn after 2 Years (Female):",
+        "Percent Completed in 3 Years:",
+        "Percent Withdrawn after 3 Years:",
+        "Percent Completed in 4 Years:",
+        "Percent Withdrawn after 4 Years:",
+        "Percent Financially Dependent:",
+        "Percent of First-Generation Students:",
+        "Percent of Parents With Middle School (highest degree):",
+        "Percent of Parents with High School (highest degree):",
+        "Percent of Parents with Post-Secondary:",
+        "Avg Family Income of Independents:",
+        "Avg Family Income of Dependents:",
+        "Median Debt of Students who Graduated:",
+        "Median Debt of Students who Didn't Graduate:",
+        "Median Debt of Dependent Students:",
+        "Median Debt of Independent Students:",
+        "Median Debt of Female Students:",
+        "Median Debt of Male Students:",
+        "Median Debt of First Generation Students:",
+        "Median Debt of Non-first Generation Students:"]
+
 if __name__ == '__main__':
-    res = getDonatedSchools()
-    print res
-    # print map(lambda x: x[GRAD_4YR], res['Harvard University'][1])
-    al = getAllRecent()
+    getSummarySheet([("Harvard University", 0.3), ("University of Colorado Boulder", 0.6)], True)
